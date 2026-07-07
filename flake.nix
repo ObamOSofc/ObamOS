e17ac480dc61555032194a4f171052ee92c25f24
ObamOSofc <obamosofc@gmail.com>
2026-07-07 16:51:21 +0300

    Security: Disabled autologin and finalized branding

--- flake.nix
+++ flake.nix
@@ -1,42 +1,46 @@
 {
-  description = "ObamOS - A Custom NixOS Distribution";
+  description = "ObamOS - The Custom OS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs }: {
     nixosConfigurations.obamos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # 1. Main System Configuration
+        # Use a minimal profile instead of the full installer
+        "${nixpkgs}/nixos/modules/profiles/all-hardware.nix"
         {
           system.stateVersion = "26.11";
           
-          # Custom Branding
+          # 1. Branding & Identity
+          networking.hostName = "obamos";
           environment.etc."os-release".text = ''
             NAME="ObamOS"
             ID=obamos
             PRETTY_NAME="ObamOS 1.0"
             VERSION="1.0"
-            HOME_URL="https://github.com/ObamOSofc/ObamOS"
           '';
 
-          # Minimal Boot Configuration
+          # 2. Shell & Prompt
+          environment.interactiveShellInit = ''
+            export PS1="ObamOS \w \$ "
+          '';
+
+          # 3. Security: Require Password (No Autologin)
+          services.getty.autologinUser = null;
+          users.users.root.initialHashedPassword = ""; # Empty for now, but you can set a custom one
+
+          # 4. Minimal System
           boot.plymouth.enable = false;
-          boot.loader.grub.configurationName = "ObamOS";
+          services.xserver.enable = false;
           
           environment.systemPackages = with nixpkgs.legacyPackages.x86_64-linux; [
             bashInteractive
             coreutils
-            hyprland
           ];
-
-          services.xserver.enable = true;
         }
-
-        # 2. Build as an ISO image (Standard NixOS method)
-        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
       ];
     };
   };
